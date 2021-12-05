e1a365705896ec30e30e450df8e54f8a9fccd6ed
akash2511 <https://github.com/akash2511>
2021-12-05 23:42:06 +0530

    feat: 🎸 Adds get my profile

--- plugins/profile/index.js
+++ plugins/profile/index.js
@@ -11,5 +11,6 @@ exports.plugin = {
     server.route(require('./routes/get_all_profiles'));
     server.route(require('./routes/get_profile_by_ids'));
     server.route(require('./routes/get_profiles_by_doctor_id'));
+    server.route(require('./routes/get_my_profile'));
   }
 };
