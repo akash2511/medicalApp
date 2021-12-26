cac17aa23816c3e857909aaafe3a33721476af58
akash2511 <akash251193@gmail.com>
2021-12-26 18:37:18 +0530

    clean up

--- plugins/emr/handlers/get_emr_by_patient_id.js
+++ plugins/emr/handlers/get_emr_by_patient_id.js
@@ -15,13 +15,14 @@ module.exports = async (request, h) => {
     })
     const last_prescription = prescriptions[prescriptions.length - 1];
     const medication = await server.methods.check_medication(last_prescription.medication_id);
-    const diet_plan = await server.methods.check_diet_plan(medication.diet_plan_id);
-    const diet_ids = diet_plan.diets.map(diet=>diet.diet_id);
-    const supplement_ids = diet_plan.supplements;
+    const diet_ids = [];
+    medication.breakfast.forEach(diet=>diet_ids.push(diet.diet_id));
+    medication.lunch.forEach(diet=>diet_ids.push(diet.diet_id));
+    medication.snack.forEach(diet=>diet_ids.push(diet.diet_id));
+    medication.dinner.forEach(diet=>diet_ids.push(diet.diet_id));
     const diets = await server.methods.check_diet(diet_ids);
-    const supplements = await server.methods.check_supplements(supplement_ids);
-    const self_management = await server.methods.check_self_management(medication.self_management_id);
-    const exercises = await server.methods.check_exercise(self_management.exercises)
+    const supplements = await server.methods.check_supplements(medication.supplement_ids);
+    const exercises = await server.methods.check_exercise(medication.exercises)
     return {
       statusCode: 201,
       message: `Emr fetched successfully`,
@@ -29,10 +30,8 @@ module.exports = async (request, h) => {
         emr,
         prescription: last_prescription,
         medication,
-        diet_plan,
         diets,
         supplements,
-        self_management,
         exercises
       }
     };
