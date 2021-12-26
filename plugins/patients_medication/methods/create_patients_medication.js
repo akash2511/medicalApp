53d015d570e74c2561e98c32d325a71bd6e6464d
akash2511 <akash251193@gmail.com>
2021-12-26 21:45:59 +0530

    clean up

--- plugins/patients_medication/methods/create_patients_medication.js
+++ plugins/patients_medication/methods/create_patients_medication.js
@@ -8,8 +8,23 @@ module.exports = {
     try {
       const medication = await functions.check_medication(medication_id);
       if(!medication) throw new Error('Medication not found');
-      delete medication._id;
-      const uploadObject = Object.assign({}, medication, {patient_id});
+      const {medication_for, drug_class, type, generic_name, brand_name, description, breakfast, lunch, snack, dinner, supplement_ids, exercises, sleep_in_min} = medication;
+      const uploadObject = {
+        patient_id,
+        medication_for, 
+        drug_class, 
+        type, 
+        generic_name, 
+        brand_name, 
+        description, 
+        breakfast, 
+        lunch, 
+        snack, 
+        dinner, 
+        supplement_ids, 
+        exercises, 
+        sleep_in_min
+      };
       const patients_medication = await PatientsMedicationModel.create(uploadObject);
       return patients_medication;
     } catch (e) {
